be39e14110b8ecd01d53f46ffcc465aa88f1faae
Michelle <Michelle@10.207.8.90>
2017-10-29 18:46:17 +0100

    - Erstentwurf Login - Auskommentieren von ".htaccess", damit loginView.php aufgerufen werden kann

--- view/loginView.php
+++ view/loginView.php
@@ -11,25 +11,32 @@ Diese Seite stellt die Login-Seite dar, bei welcher sich bereits registrierte Us
         <?php
         // Session starten oder vorhandene übernehmen
         session_start();
-
+        
+        if(isset($_POST['sub'])) {
+            include_once './../validator/validate.php';
+            if(validateLogin()) {
+                $userID = intval($_POST['userID']);
+                $password = $_POST['password'];
+                if($userID == 1 && $password == "test") {
+                    $_SESSION['login'] = true;
+                }
+            }
+        }
+        
         // Session beenden -> session_destroy() reicht nicht
-        session_destroy();
+        //session_destroy();
 
         // If it's desired to kill the session, also delete the session cookie.
         // Note: This will destroy the session, and not just the session data!
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]
-            );
-        }
+//        if (ini_get("session.use_cookies")) {
+//            $params = session_get_cookie_params();
+//            setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]
+//            );
+//        }
         ?>
         <h3>Login</h3>
-        <form onsubmit="return validateLogin()" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
             <table>
-                <tr>
-                    <td>icon</td><td>Name</td>
-                    <td><input type="text" name="name" value="" /><br/></td>
-                </tr>
                 <tr>
                     <td>icon</td><td>User-ID</td>
                     <td><input type="text" name="userID" value="" /><br/></td>
@@ -41,7 +48,7 @@ Diese Seite stellt die Login-Seite dar, bei welcher sich bereits registrierte Us
                 <tr>
                     <td></td>
                     <td></td>
-                    <td><input type="submit" value="einloggen" /> <input type="reset" value="zurücksetzen" /></td>
+                    <td><input type="submit" value="einloggen" name="sub" /> <input type="reset" value="zurücksetzen" /></td>
                 </tr>
                 <tr>
                 <br>
--- view/registerView.php
+++ view/registerView.php
@@ -4,12 +4,25 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
+<?php
+echo "hallo";
+session_start();
+
+if(!isset($_SESSION['login']) || $_SESSION['login'] == false) {
+    echo "nicht eingeloggt";
+    header("Location: http://localhost/TeamAmused/view/loginView.php");
+}
+?> 
+
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <p>test</p>
         <?php
         // put your code here
         ?>
