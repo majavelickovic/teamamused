57c4d839482d4d14174c3e41e5bc7e1a0d1eb240
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-03 12:23:50 +0100

    dummy-files angelegt für die einzelnen view -> von sandra dropbox

---
+++ view/exist_participant.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<?php
+
+?>
+
+<html>
+	<head>
+		<title>bestehende Teilnehmer</title>
+		<link rel="stylesheet" href="../design/styles.css">
+	</head>
+	<body>
+		<div id="block">
+			<div id="part1">
+				<h1>Reiseverwaltung</h1>
+				<p>bestehende Teilnehmer</p>
+				<!--<form>
+					<label>User-ID</label>
+					<input type="text" name="uname" required></br></br>
+					<label>Passwort</label>
+					<input type="password" name="pw" required></br></br>
+					<button type="button" name="login">einloggen</button>
+					<button type="button" name="reset">zurücksetzen</button>
+				</form>-->
+			</div>
+		</div>
+	</body>
+</html>
