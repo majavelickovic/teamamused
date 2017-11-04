a0d7b8480433378250edc8e1b6124684e8cc12d6
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-04 15:20:33 +0100

    test mit router

--- router/Router.php
+++ router/Router.php
@@ -50,7 +50,6 @@ class Router
 
     public static function errorHeader() {
         header("HTTP/1.0 404 Not Found");
-        echo "TEST MAJA";
     }
 
     public static function redirect($redirect_path) {
