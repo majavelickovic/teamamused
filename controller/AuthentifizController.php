ed0d81f7da27a7a00adff59afeeaf382b0da48cd
Michelle <Michelle@10.207.8.90>
2017-11-05 13:31:11 +0100

    AuthentifizierungsController erstellt

---
+++ controller/AuthentifizController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreas.martin
+ * Date: 09.10.2017
+ * Time: 08:30
+ */
+
+namespace controller;
+
+use service\Service;
+
+class AuthController
+{
+
+    public static function authenticate(){
+        if (isset($_SESSION['login'])) {
+            if(Service::getInstance()->validateToken($_SESSION['login']['token'])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function login(){
+        if(Service::getInstance()->verifyUser($_POST['userId'],$_POST['password']))
+        {
+            $_SESSION['user']['token'] = Service::getInstance()->issueToken();
+        }
+    }
+
+}
