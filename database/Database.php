8b3d9c29395385c9e69b3504261f0c89e553855d
Michelle <Michelle@10.207.8.90>
2017-11-05 13:03:25 +0100

    connect()-Methode angepasst

--- database/Database.php
+++ database/Database.php
@@ -5,13 +5,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+namespace database;
+
+use PDO;
 
 class Database
 {
     public static function connect()
     {
-        $pdo = new PDO('mysql:host=jdbc:postgresql://ec2-176-34-111-152.eu-west-1.compute.amazonaws.com:5432/dbabpfc2jh2nbo;dbname=Heroku DB','root', '');
-
+        $host = "ec2-176-34-111-152.eu-west-1.compute.amazonaws.com";
+        $port = "5432";
+        $db = "dbabpfc2jh2nbo";
+        $username = "ztdczgwqtyarlj";
+        $password = "d314d8b083f6178b1a5be24bb6e57c118b74d436179cbedeff73f7a843344fdc";
+        
+        $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db;user=$username;password=$password");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
 
