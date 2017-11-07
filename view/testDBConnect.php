e9927593e4a8927cf80d01e6bdcb31d286539228
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-07 17:00:59 +0100

    test db connection with separate file

--- view/testDBConnect.php
+++ view/testDBConnect.php
@@ -12,7 +12,6 @@
 
     $query = $pdo->query("SELECT beschreibung FROM rechnungsart"); // Run your query
     
-    echo '<select name="rgart">'; // Open your drop down box
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
          echo "wert: ".$row['beschreibung']. "<br>";
     }
