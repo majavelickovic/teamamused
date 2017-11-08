0522a7cb68f07e98e8414611c7a268b965bd0bed
Michelle <Michelle@10.207.8.90>
2017-11-08 20:48:32 +0100

    Refactoring userID

--- dao/LoginDAO.php
+++ dao/LoginDAO.php
@@ -21,9 +21,8 @@ class LoginDAO {
 	public function create(Login $login) {
             $pdo = Database::connect();
             $statement = $pdo->prepare(
-                    "INSERT INTO login (userid, benutzername, passwort, vorname, nachname, rolle)
-                        VALUES (:userid, :benutzername, :passwort, :vorname, :nachname, 1)");
-            $statement->bindValue(':userid', $login->getUserId());
+                    "INSERT INTO login (benutzername, passwort, vorname, nachname, rolle)
+                        VALUES (:benutzername, :passwort, :vorname, :nachname, 1)");
             $statement->bindValue(':benutzername', $login->getBenutzername());
             $statement->bindValue(':passwort', $login->getPasswort());
             $statement->bindValue(':vorname', $login->getVorname());
@@ -35,54 +34,64 @@ class LoginDAO {
 	/**
 	 * Liest einen registrierten Mitarbeiter aus der Tabelle "login"
 	 */
-	public function read($userid) {
-        $pdo = Database::connect();
-        $statement = $pdo->prepare(
-            "SELECT * FROM login WHERE userid = :userid;");
-        $statement->bindValue(':userid', $userid);
-        $statement->execute();
-        return $statement->fetchAll(\PDO::FETCH_CLASS, "Login")[0];
+	public function read($benutzername) {
+            $pdo = Database::connect();
+            $statement = $pdo->prepare(
+                "SELECT * FROM login WHERE benutzername = :benutzername;");
+            $statement->bindValue(':benutzername', $benutzername);
+            $statement->execute();
+            return $statement->fetchAll(\PDO::FETCH_CLASS, "Login")[0];
 	}
 
 	/**
 	 * Aktualisiert einen registrierten Mitarbeiter in der Tabelle "login"
 	 */
 	public function update(Login $login) {
-        $pdo = Database::connect();
-        $statement = $pdo->prepare(
-            "UPDATE login SET benutzername = :benutzername, passwort = :passwort, vorname = :vorname, nachname = :nachname, rolle = 1
-            WHERE userid = :userid");
-        $statement->bindValue(':userid', $login->getUserId());
-        $statement->bindValue(':benutzername', $login->getBenutzername());
-        $statement->bindValue(':passwort', $login->getPasswort());
-        $statement->bindValue(':vorname', $login->getVorname());
-        $statement->bindValue(':nachname', $login->getNachname());
-        $statement->execute();
-        return $this->read($login->getUserId());
+            $pdo = Database::connect();
+            $statement = $pdo->prepare(
+                "UPDATE login SET benutzername = :benutzername, passwort = :passwort, vorname = :vorname, nachname = :nachname, rolle = 1
+                WHERE benutzername = :benutzername");
+            $statement->bindValue(':benutzername', $login->getBenutzername());
+            $statement->bindValue(':passwort', $login->getPasswort());
+            $statement->bindValue(':vorname', $login->getVorname());
+            $statement->bindValue(':nachname', $login->getNachname());
+            $statement->execute();
+            return $this->read($login->getBenutzername());
 	}
 
 	/**
 	 * Löscht einen registrierten Mitarbeiter aus der Tabelle "login"
 	 */
 	public function delete(Login $login) {
-        $pdo = Database::connect();
-        $statement = $pdo->prepare(
-            "DELETE FROM login
-            WHERE userid = :userid");
-        $statement->bindValue(':userid', $login->getUserId());
-        $statement->execute();
+            $pdo = Database::connect();
+            $statement = $pdo->prepare(
+                "DELETE FROM login
+                WHERE benutzername = :benutzername");
+            $statement->bindValue(':benutzername', $login->getBenutzername());
+            $statement->execute();
 	}
 
 	/**
 	 * noch überarbeiten
 	 */
-	public function findByUserId($userid) {
+	public function findByBenutzername($benutzername) {
             $pdo = Database::connect();
             $statement = $pdo->prepare(
-                "SELECT * FROM login WHERE userid = :userid");
-            $statement->bindValue(':userid', $userid);
+                "SELECT * FROM login WHERE benutzername = :benutzername");
+            $statement->bindValue(':benutzername', $benutzername);
             $statement->execute();
-            return $statement->fetchAll()[0];
+            $row = $statement->fetchAll()[0];
+            if(isset($row)) {
+                $login = new Login();
+                $login->setBenutzername($row['benutzername']);
+                $login->setVorname($row['vorname']);
+                $login->setNachname($row['nachname']);
+                return $login;
+            } else {
+                throw new Exception('Unbekannter Benutzername.');
+            }
+            
+            //return $statement->fetchAll()[0];
 //            return $statement->fetchAll(\PDO::FETCH_CLASS, "domain\Login")[0];
         }
 
