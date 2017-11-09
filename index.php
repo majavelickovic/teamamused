b1ddcf220f744503f3dd5b1945ceb8be914ce268
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-09 19:09:56 +0100

    test neue rechnung auf db erstellen

--- index.php
+++ index.php
@@ -88,7 +88,14 @@ Router::route("GET", "/rechnung/neu", function () {
 });
 
 Router::route("POST", "/rechnung/neu", function () {
-                $rechnungDAO = new \dao\RechnungDAO();
+            echo "rgid:".$rechnungDAO->getNewRgID();
+            echo "reise:".$_POST["reise"];
+            echo "rgart:".$_POST["rgart"];       
+            echo "beschreibung:".$_POST["beschreibung"];
+            echo "kosten:".$_POST["kosten"];
+            echo "doku:".$_POST["dokument"];
+    
+    /* $rechnungDAO = new \dao\RechnungDAO();
             // Rechnungsinhalte bestimmen
             $rechnung = new \domain\Rechnung();
             $neu_id = $rechnungDAO->getNewRgID();
@@ -98,7 +105,7 @@ Router::route("POST", "/rechnung/neu", function () {
             $rechnung->setBeschreibung($_POST["beschreibung"]);
             $rechnung->setKosten($_POST["kosten"]);
             $rechnung->setDokument($_POST["dokument"]);
-            $rechnungDAO->create($rechnung);
+            $rechnungDAO->create($rechnung);*/
     if(controller\RechnungController::neueRechnung() != false){
         Router::redirect("/rechnung/neu");
     } else {
