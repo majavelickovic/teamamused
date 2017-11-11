2a888dc68afc76f034d1962593f6abcba0892c9b
Michelle <Michelle@10.207.8.90>
2017-11-11 13:13:28 +0100

    Fehlerhandling

--- dao/LoginDAO.php
+++ dao/LoginDAO.php
@@ -88,7 +88,7 @@ class LoginDAO {
                 $login->setNachname($row['nachname']);
                 return $login;
             } else {
-                echo('Unbekannter Benutzername.');
+                echo("Unbekannter Benutzername.");
             }
             
             //return $statement->fetchAll()[0];
