75202a06f01659b924f2b1eb34ca3211c2f71a7a
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-11 17:20:17 +0100

    Arbeit an "Rechnung suchen/ansehen"

--- view/single_calculation.php
+++ view/single_calculation.php
@@ -6,11 +6,11 @@ $rg_id = $rg_id;
 print_r("maja test");
 print_r($rg_id);
 print_r($_GET['id']);
-$pdo = Database::connect();           
+/*$pdo = Database::connect();           
 $query = $pdo->query("SELECT rechnung.rg_id, reise_rechnung.reise_id, rechnung.rechnungsart, rechnung.kosten, rechnung.beschreibung, rechnung.dokument
                    FROM rechnung INNER JOIN reise_rechnung ON rechnung.rg_id=reise_rechnung.rg_id WHERE rechnung.rg_id = :rg_id;");
 $query->bindValue(':rg_id', $rg_id);
-$rg = $query->fetchAll(PDO::FETCH_CLASS, "Rechnung");
+$rg = $query->fetchAll(PDO::FETCH_CLASS, "Rechnung");*/
 
 /*
  * View, um eine einzelne Rechnung anzusehen / zu bearbeiten
