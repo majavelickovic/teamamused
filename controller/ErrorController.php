226a7d16329d41bee9e5c71038f7c4a7688bbb18
Michelle <Michelle@10.207.8.90>
2017-11-18 16:07:01 +0100

    ErrorController zur Steuerung der Error-Seiten

--- controller/ErrorController.php
+++ controller/ErrorController.php
@@ -1,4 +1,4 @@
-<?php
+<?php //
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -6,3 +6,20 @@
  * and open the template in the editor.
  */
 
+namespace controller;
+
+use view\view as View;
+
+class ErrorController
+{
+
+    public static function error404View(){
+      echo (new View("error404.php"))->render();
+    }
+    
+    public static function error403View(){
+      echo (new View("error403.php"))->render();
+    }
+    
+}
+?>
