f8c460d197bc57a544e2bf7cb6b1a0a52afa531a
Michelle <Michelle@10.207.8.90>
2017-11-19 18:23:08 +0100

    noch schöner gestalten

--- view/error403.php
+++ view/error403.php
@@ -15,7 +15,7 @@ Diese Seite stellt die 403-Error-Seite dar.
                     <h1>403 Error!  <i class="em em-point_up"></i></h1>
                     <h2>Du hast keinen Zugriff! Melde dich zuerst an...</h2>
                     <img src="<?php echo $GLOBALS["ROOT_URL"]; ?>/design/pictures/403-forbidden.jpg">
-                    <br>
+                    <br><br>
                     <a href="<?php echo $GLOBALS["ROOT_URL"] . "/login" ?>">Hier gelangst du zur Login-Seite</a>
                 </div>
             </div>
