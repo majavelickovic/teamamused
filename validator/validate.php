37f671c7da339359d0e65cd3b23e2fa93b37e872
Michelle <Michelle@10.207.8.90>
2017-11-19 16:55:45 +0100

    Weiterarbeit an der Serverseitigen Validierung

--- validator/validate.php
+++ validator/validate.php
@@ -23,106 +23,94 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 // Validierung der Register-Seite bezüglich Vollständigkeit der Inputfelder
-$benutzername = "";
 $benutzernameRegisterError = "";
-$vorname = "";
 $vornameRegisterError = "";
-$nachname = "";
 $nachnameRegisterError = "";
-$password1 = "";
 $password1RegisterError = "";
-$password2 = "";
 $password2RegisterError = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["benutzername"])) {
     $benutzernameRegisterError = "Die Eingabe eines Benutzernamens ist erforderlich!";
   } else {
-    $benutzername = validateBenutzername($_POST["benutzername"]);
+    validateBenutzername($_POST["benutzername"]);
   }
   
   if (empty($_POST["vorname"])) {
     $vornameRegisterError = "Die Eingabe eines Vornamens ist erforderlich!";
   } else {
-    $vorname = validateVorname($_POST["vorname"]);
+    validateVorname($_POST["vorname"]);
   }  
 
   if (empty($_POST["nachname"])) {
     $nachnameRegisterError = "Die Eingabe eines Nachnamens ist erforderlich!";
   } else {
-    $nachname = validateNachname($_POST["nachname"]);
+    validateNachname($_POST["nachname"]);
   }    
   
   if (empty($_POST["password1"])) {
     $password1RegisterError = "Die Eingabe eines Passwortes ist erforderlich!";
   } else {
-    $password1 = validatePassword1($_POST["password1"]);
+    validatePassword1($_POST["password1"]);
   }
   
   if (empty($_POST["password2"])) {
     $password2RegisterError = "Die Bestätigung des Passwortes ist erforderlich!";
   } else {
-    $password2 = validatePassword2($_POST["password2"]);
+    validatePassword2($_POST["password1"],$_POST["password2"]);
   }  
   
 }
 
 $benutzernameUniqueError = "";
+$vornameCharError = "";
+$nachnameCharError = "";
+$password1CharError = "";
+$password1ConfirmError = "";
 
+// Prüft, ob der Benutzername eindeutig ist
 function validateBenutzername($benutzername){
-    // Formale Prüfung
-    // TODO
-    
     // Gibt true zurück, falls es den Benutzernamen bereits gibt
     if (Service::getInstance()->uniqueBenutzername($benutzername)){
-        $benutzernameUniqueError = "";
-    } //TODO: und dann?
+        $benutzernameUniqueError = "Den Benutzernamen gibt es bereits. Wählen Sie einen anderen.";
+    }
 }
+
+// Prüft, ob der Vorname den formalen Vorgaben entspricht
 function validateVorname($vorname){
-    // Formale Vorgaben prüfen
+    // preg_match gibt 1 zurück, falls der String nur Buchstaben und Leerzeichen enthält
+    if(1 !== preg_match('/^[a-zA-Z ]$/', $vorname)){
+        $vornameCharError = "Der Vorname darf nur Buchstaben enthalten!";
+    }
     
 }
 
+// Prüft, ob der Nachname den formalen Vorgaben entspricht
 function validateNachname($nachname){
-    // Formale Vorgaben prüfen
+    // preg_match gibt 1 zurück, falls der String nur Buchstaben und Leerzeichen enthält
+    if(1 !== preg_match('/^[a-zA-Z ]$/', $nachname)){
+        $nachnameCharError = "Der Nachname darf nur Buchstaben enthalten!";
+    }
     
 }
 
+// Prüft, ob das Passwort den formalen Vorgaben entspricht
 function validatePassword1($password1){
-    // Formale Vorgaben prüfen
+    // preg_match gibt 1 zurück, falls der String nur Buchstaben und Leerzeichen enthält
+    if(1 === preg_match('/^[a-zA-Z ]$/', $password1)){
+        $password1CharError = "Das Passwort darf nicht nur aus Buchstaben bestehen!";
+    }
     
 }
 
-function validatePassword2($password2){
-    // Übereinstimmung mit password1 prüfen
+// Prüft, ob password1 mit password2 übereinstimmt
+function validatePassword2($password1, $password2){
+    if (password1 !== password2){
+        $password1ConfirmError = "Das bestätigte Passwort stimmt nicht überein!";
+    }
     
 }
 
-function validateLogin() {
-    $complete = isset($_POST['benutzername']) AND isset($_POST['password']);
-    $type = $complete AND is_int(intval($_POST['benutzername']));
 
-    if ($type) {
-        return true;
-    } else {
-        echo "Eingabe ist nicht korrekt";
-        return false;
-        //Meldung ausgeben, ob nicht vollständig oder Typ nicht stimmt
-    }
-}
-
-function validateRegister() {
-    $complete = isset($_POST['name']) AND isset($_POST['benutzername']) AND isset($_POST['password']);
-    $type = $complete AND is_int(intval($_POST['benutzername']));
-
-    if ($type) {
-        echo "Eingabe ist korrekt";
-        return true;
-    } else {
-        echo "Eingabe ist nicht korrekt";
-        return false;
-        //Meldung ausgeben, ob nicht vollständig oder Typ nicht stimmt
-    }
-}
 
 ?>
