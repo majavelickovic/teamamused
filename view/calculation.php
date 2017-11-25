086429f6583af1faf42e81132717ee5cc4ce7e42
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-11-25 13:24:11 +0100

    korrektur link

--- view/calculation.php
+++ view/calculation.php
@@ -22,14 +22,14 @@ Diese Seite stellt die Rechnungs-Seite dar.
                 <div id="blockleft">
                     <table>
                         <tr>
-                        <a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/neu" ?>" style="text-decoration: none;"><td><img src="../design/pictures/plus.png"></td><td>neue Rechnung hinzufügen</td></a>
+                            <td><a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/neu" ?>"><img src="../design/pictures/plus.png"></a></td><td><a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/neu" ?>" style="text-decoration: none;">neue Rechnung hinzufügen</a></td>
                         </tr>
                     </table>
                 </div>
                 <div id="blockright">
                     <table>
                         <tr>
-                            <a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/bestehend" ?>" style="text-decoration: none;"><td><img src="../design/pictures/search.png"></td><td>bestehende Rechnung anzeigen</td></a>
+                            <td><a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/bestehend" ?>"><img src="../design/pictures/search.png"></a></td><td><a href="<?php echo $GLOBALS["ROOT URL"] . "/rechnung/bestehend" ?>" style="text-decoration: none;">bestehende Rechnung anzeigen</a></td>
                         </tr>
                     </table>
 
