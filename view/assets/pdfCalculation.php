829750281b2968f86a70fc7cd245f48f07ca1514
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-03 17:01:09 +0100

    test Schlussabrechnung

--- view/assets/pdfCalculation.php
+++ view/assets/pdfCalculation.php
@@ -20,21 +20,21 @@ $pdf->SetFont('Arial', '', 10);
 $zinsfuss = 1.5;
 
 // Lese Daten für Schlussabrechnung
-$pdo = Database::connect();           
+$pdo = database\Database::connect();           
 $statement = $pdo->prepare("SELECT beschreibung FROM reise WHERE reise = :reise;");
 $statement->bindValue(':reise', $_POST['reise']);
 $statement->execute();
 while ($row = $statement->fetch()){
     $reisename = $row['beschreibung'];
 }
-$dataSchlussabrechnung = controller\RechnungController::readFinalBilling($_POST['reise']);
+/*$dataSchlussabrechnung = controller\RechnungController::readFinalBilling($_POST['reise']);
 $pdf->SetTitle($reisename);
 // Tabelle
 
 for ($rows=0; array_count_values($dataSchlussabrechnung)-1; $rows=$row+1)
-{
+{*/
 /* Einstellung für unterschiedliche Zeilenfarbe */
-    if ($rows%2==0)
+   /* if ($rows%2==0)
     {
 	$pdf->SetFillColor(192,192);
     }else{
@@ -44,7 +44,7 @@ for ($rows=0; array_count_values($dataSchlussabrechnung)-1; $rows=$row+1)
 $pdf->Cell(120,5,array_column($dataSchlussabrechnung($rows,0)),"LR",0,"C",1);
 $pdf->Cell(30,5,"CHF ".number_format(array_columns($dataSchlussabrechnung($rows,1)), 1),"LR",0,"R",1);
 $pdf->Ln();
-}
+}*/
 
 $pdf->Output();     
 ?>
