77688121af5d8e14ff3d78a9b89b23d13a25f51b
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-03 19:01:37 +0100

    test Schlussabrechnung

--- view/assets/pdfCalculation.php
+++ view/assets/pdfCalculation.php
@@ -64,7 +64,7 @@ class PDF extends FPDF {
      // nach rechts gehen
      $this->Cell(80);
      // Titel
-     $this->Cell(100,10,$reisename,1,0,'C');
+     $this->Cell(200,10,'Schlussabrechnung - ' . $reisename,1,0,'L');
      // Zeilenumbruch
      $this->Ln(20);
   } 
@@ -79,14 +79,89 @@ class PDF extends FPDF {
      // Seitenzahl
      $this->Cell(0,10,'Seite '.$this->PageNo().'/{nb}',0,0,'C');
   }
+  
+  // Simple table
+  function BasicTable($header,$data)
+  {
+    // Header
+    foreach($header as $col)
+     $this->Cell(40,7,$col,1);
+    $this->Ln();
+    // Data
+    foreach($data as $row)
+    {
+     foreach($row as $col)
+     $this->Cell(40,6,$col,1);
+     $this->Ln();
+    }
+  }
+
+  // Better table
+  function ImprovedTable($header,$data)
+  {
+    // Column widths
+    $w=array(40,35,40,45);
+    // Header
+    for($i=0;$i<count($header);$i++)
+     $this->Cell($w[$i],7,$header[$i],1,0,'C');
+    $this->Ln();
+    // Data
+    foreach($data as $row)
+    {
+     $this->Cell($w[0],6,$row[0],'LR');
+     $this->Cell($w[1],6,$row[1],'LR');
+     $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
+     $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
+     $this->Ln();
+    }
+    // Closure line
+    $this->Cell(array_sum($w),0,'','T');
+  }
+
+  // Colored table
+  function FancyTable($header,$data)
+  {
+    // Colors, line width and bold font
+    $this->SetFillColor(255,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // Header
+    $w=array(40,35,40,45);
+    for($i=0;$i<count($header);$i++)
+     $this->Cell($w[$i],7,$header[$i],1,0,'C',1);
+    $this->Ln();
+    // Color and font restoration
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Data
+    $fill=0;
+    foreach($data as $row)
+    {
+     $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+     $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+     $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
+     $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
+     $this->Ln();
+     $fill=!$fill;
+    }
+    $this->Cell(array_sum($w),0,'','T');
+  }
+  
 }
 
 $pdf=new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-  $pdf->Cell(50,10,'Zeilennummer '.$i,0,1);
-  $pdf->Cell(50,10,'text2',0,1);
+$pdf->SetFont('Arial','',14);
+$pdf->AddPage();
+$dataSchlussabrechnung = controller\RechnungController::readFinalBilling($_POST['reise']);
+$pdf->BasicTable($header,$dataSchlussabrechnung);
+$pdf->AddPage();
+$pdf->ImprovedTable($header,$dataSchlussabrechnung);
+$pdf->AddPage();
+$pdf->FancyTable($header,$dataSchlussabrechnung);
 $pdf->Output();    
 ?>
