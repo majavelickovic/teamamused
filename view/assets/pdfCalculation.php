5a4e37f9540689e47907afe67941383a882b45b1
Michelle <Michelle@10.207.8.90>
2017-12-03 13:57:39 +0100

    PDF-Beispiel mit Tabelle

--- view/assets/pdfCalculation.php
+++ view/assets/pdfCalculation.php
@@ -1,9 +1,45 @@
 <?PHP
 include('FPDF/fpdf.php');   //Pfad zu fpdf.php
 
-$pdf = new FPDF();  // neues Objekt der Klasse FPDF
-$pdf->AddPage();    // erzeugt eine Seite
-$pdf->SetFont('Arial', 'B', 16);   
-$pdf->Cell(40,10,'Schlussrechnung');
-$pdf->Output();     // Ausgabe des PDF-Objekts
+$pdf = new FPDF();  
+$pdf->AddPage();  
+
+/*Einstellung der Überschrift */  
+$pdf->SetFont('Arial', 'B', 10);
+$pdf->SetLineWidth(0.2);
+$pdf->SetDrawColor(0,0,0);
+$pdf->SetFillColor(192,192,192);
+
+// Überschrift
+$pdf->Cell(30,5,"Kapital:","LTR",0,"C",1);
+$pdf->Cell(50,5,"Zinssatz in [%]:","1",0,"C",1);
+$pdf->Cell(70,5,"Ertrag:","1",0,"C",1);
+$pdf->Ln();
+
+/* Einstellungen der Tabelle */
+$pdf->SetFont('Arial', '', 10);
+$zinsfuss = 1.5;
+
+// Tabelle
+
+for ($kapital=100; $kapital<=800; $kapital=$kapital+100)
+{
+/* Einstellung für unterschiedliche Zeilenfarbe */
+	if ($kapital%200==0)
+	{
+	$pdf->SetFillColor(192,192,192);
+	}
+	else
+	{
+	$pdf->SetFillColor(224,224,224);
+	}
+
+$pdf->Cell(30,5,"Fr. ".$kapital,"LR",0,"C",1);
+$pdf->Cell(50,5,$zinsfuss,"LR",0,"C",1);
+$ertrag=$kapital + $kapital*$zinsfuss/100;
+$pdf->Cell(70,5,"Fr. ".number_format($ertrag,1),"LR",0,"R",1);
+$pdf->Ln();
+}
+
+$pdf->Output();     
 ?>
