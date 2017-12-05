33b7eaae385ddc71de57fa94514ad88f6c41c4a6
Sandra Bodack <Sandra Bodack@10.207.22.116>
2017-12-05 21:54:54 +0100

    participantChoiceView() und journeyChoiceView() ergänzt

--- controller/TeilnehmerController.php
+++ controller/TeilnehmerController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Controller für die Teilnehmer-View
  */
@@ -8,59 +9,42 @@ namespace controller;
 use view\view as View;
 use service\Service;
 
-$del_teilnehmer_id = $_GET['del_teilnehmer_id'];
-if($del_teilnehmer_id > 0){
-    TeilnehmerController::deleteParticipant($del_teilnehmer_id);
-}
-
-class TeilnehmerController
-{
-
+class TeilnehmerController {
     /*
      * Übernimmt die Angaben aus dem Teilnehmerformular und gibt diese an die Service-Klasse weiter
      * Erhält aus der Service-Klasse einen Boolean zurück bei erfolgreichem Ändern/Hinzufügen eines Teilnehmers
      */
-    public static function newParticipant(){
+
+    public static function newParticipant() {
         return Service::getInstance()->createParticipant(
-                $_POST["reise_id"],
-                $_POST["reisetitel"],
-                $_POST["vorname"],
-                $_POST["nachname"],
-                $_POST["geburtsdatum"]);
+                        $_POST["reise_id"], $_POST["titel"], $_POST["vorname"], $_POST["nachname"], $_POST["geburtsdatum"]);
     }
-    
-        public static function readParticipant(){
+
+    public static function readParticipant() {
         return Service::getInstance()->readParticipant(
-                $_POST["reise_id"],
-                $_POST["reisetitel"],
-                $_POST["teilnehmer_id"],
-                $_POST["vorname"],
-                $_POST["nachname"],
-                $_POST["geburtsdatum"]);
+                        $_POST["reise_id"], $_POST["titel"], $_POST["teilnehmer_id"], $_POST["vorname"], $_POST["nachname"]);
     }
-    
-        public static function updateParticipant(){
+
+    public static function updateParticipant() {
         return Service::getInstance()->updateParticipant(
-                $_POST["reise_id"],
-                $_POST["reisetitel"],
-                $_POST["teilnehmer_id"],
-                $_POST["vorname"],
-                $_POST["nachname"],
-                $_POST["geburtsdatum"]);
+                        $_POST["reise_id"], $_POST["titel"], $_POST["teilnehmer_id"], $_POST["vorname"], $_POST["nachname"], $_POST["geburtsdatum"]);
     }
-    
-        public static function deleteParticipant(){
+
+    public static function deleteParticipant($teilnehmer_id) {
         return Service::getInstance()->deleteParticipant(
-                $_POST["teilnehmer_id"],
-                $_POST["vorname"],
-                $_POST["nachname"]);
+                        $teilnehmer_id);
     }
-    
-    public static function participantShowView(){
+
+    public static function participantShowView() {
         echo (new View("exist_participant.php"))->render();
     }
 
-    public static function participantAddView(){
+    public static function participantAddView() {
         echo (new View("new_participant.php"))->render();
     }
+
+    public static function participantChoiceView() {
+        echo (new View("participant.php"))->render();
+    }
+
 }
