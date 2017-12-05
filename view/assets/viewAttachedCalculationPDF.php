118a7a203f175f7766ca6063b9919293e540acfa
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-05 22:59:12 +0100

    Test für PDF-Anzeige nach Upload

--- view/assets/viewAttachedCalculationPDF.php
+++ view/assets/viewAttachedCalculationPDF.php
@@ -6,11 +6,5 @@
  */
 
 $file = service\Service::getInstance()->getAttachedPDFInvoice($_GET['rg_id']);
-/*header('Content-type: application/pdf');
-echo file_get_contents('data:application/pdf;base64,'.base64_encode($file));*/
-
-include('FPDF/fpdf.php');   //Pfad zu fpdf.php
-
-$pdf = new PDF();
-$pdf->LoadData($file);
-$pdf->Output();
+header('Content-type: application/pdf');
+echo file_get_contents('data:application/pdf;base64,'.base64_encode($file));
