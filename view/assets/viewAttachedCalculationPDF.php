0df7dd284a736f4a057ca8109e7fbeacaf0abfa1
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-06 20:01:32 +0100

    Test für PDF-Anzeige nach Upload

--- view/assets/viewAttachedCalculationPDF.php
+++ view/assets/viewAttachedCalculationPDF.php
@@ -5,6 +5,15 @@
  * @author Maja Velickovic
  */
 
-$file = service\Service::getInstance()->getAttachedPDFInvoice($_GET['rg_id']);
+
+            $pdo = Database::connect();           
+            $statement = $pdo->prepare("SELECT pdf_object FROM rechnung where rg_id = 38");
+           // $statement->bindValue(':rg_id', $rg_id);
+            $statement->execute();
+            foreach($statement as $result){
+                $file= pg_unescape_bytea($result['pdf_object']);
+            }
+        
+//$file = service\Service::getInstance()->getAttachedPDFInvoice($_GET['rg_id']);
 header("Content-type: application/pdf"); 
 print $file;  
