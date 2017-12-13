c555d78f24a70a4a207af6af00823d68d1dd9000
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-13 17:59:58 +0100

    test rg-anzeige in pdf

--- view/assets/viewAttachedCalculationPDF.php
+++ view/assets/viewAttachedCalculationPDF.php
@@ -7,11 +7,11 @@
 use database\Database;
 
             $pdo = Database::connect();           
-            $statement = $pdo->query("SELECT pdf_object FROM rechnung where rg_id = 48");
+            $statement = $pdo->query("SELECT encode(pdf_object::bytea, 'escape') FROM rechnung where rg_id = 48");
            // $statement->bindValue(':rg_id', $rg_id);
             $file = "";
             while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                $file .= $row['pdf_object'];
+                $file .= $row['encode'];
             }
 
         
