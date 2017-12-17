5ef78a34767c88e4d45b5e7a516c401209db77b1
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2017-12-17 13:52:19 +0100

    test mit änderung pdf bei bestehender rechnung

--- view/single_calculation.php
+++ view/single_calculation.php
@@ -113,10 +113,10 @@ if($rg->getReise() == ""){
                             <tr>
                                 <td>Dokument</td>
                                 <td>
-                                    <input id="FileInput" type="text" name="dokument" value="<?php echo $rg->getDokument();?>" style="width:300px;" disabled/>
+                                    <input id="dokument" type="text" name="dokument" value="<?php echo $rg->getDokument();?>" style="width:300px;" disabled/>
                                 </td>
                                 <td>
-                                    <a href="#"><img src="../design/pictures/edit.png" onclick="document.getElementById('FileInput').type='file';document.getElementById('FileInput').disabled=false;document.getElementByID('FileInput').name = 'dokument';"></a>
+                                    <a href="#"><img src="../design/pictures/edit.png" onclick="document.getElementById('dokument').type='file';document.getElementById('dokument').disabled=false;"></a>
                                     <a href="#"><img src="../design/pictures/search.png" onclick="window.open('/showSingleCalcPDF?rg_id=<?php echo $_GET['id'];?>', 'Anzeige PDF')"></a>
                                 </td>
                             </tr>
